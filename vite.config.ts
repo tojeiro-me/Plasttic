047798b999e6d83aab7f2ff63133d4f458ea88ce
Goncalo Tojeiro <dev@tojeiro.me>
2022-09-08 15:42:16 +0100

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve('/src'),
-    },
-  },
-  define: {
-    'process.env': {},
-  },
+  publicDir: 'src',
 });
