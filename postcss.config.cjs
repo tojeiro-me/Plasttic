d78dc62aa27605752bf4fd511ad969b7d49f692d
Goncalo Tojeiro <dev@tojeiro.me>
2022-11-02 15:16:29 +0000

    v1.4.4

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
   plugins: {
     autoprefixer: {},
     'postcss-preset-env': {},
-    //cssnano: {},
+    cssnano: {},
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [],
   root: 'src',
-  build: {
-    outDir: '../public/',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
-  },
+  // build: {
+  //   outDir: '../public/',
+  //   emptyOutDir: true,
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: '[name].js',
+  //       chunkFileNames: '[name].js',
+  //       assetFileNames: '[name].[ext]',
+  //     },
+  //   },
+  // },
 });
