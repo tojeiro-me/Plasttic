bc931526005df3e3c613928bec4d7450bd58c791
Goncalo Tojeiro <dev@tojeiro.me>
2023-05-20 17:55:25 +0100

    v2.0: added @csstools/postcss-global-data

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,17 @@ module.exports = {
   plugins: [
     require('postcss-import')({ path: 'src/assets/css' }),
     require('postcss-preset-env')({ stage: 2 }),
+    require('@csstools/postcss-global-data')({ files: [
+      'src/assets/css/00_reset.css',
+      'src/assets/css/10_utility.css',
+      'src/assets/css/20_components.css',
+      'src/assets/css/30_tokens.css',
+      'src/assets/css/40_atoms.css',
+      'src/assets/css/50_molecules.css',
+      'src/assets/css/60_organisms.css',
+      'src/assets/css/70_templates.css',
+      'src/assets/css/80_pages.css'
+    ] }),
     require('postcss-custom-properties'),
     require('postcss-custom-media'),
     require('postcss-media-minmax'),
