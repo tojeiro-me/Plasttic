ea3eee582bf9bc770e2096082de32ee83ad05392
tojeiro-me <tojeiro-me@users.noreply.github.com>
2023-06-15 22:36:34 +0000

    [Bot] Update package

---
+++ package/browser-sync.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  // Certificates for dev:ssl
+    https: {
+      key: 'certs/key.pem',
+      cert: 'certs/cert.pem',
+    },
+};
---
+++ package/src/assets/js/modules/plasttic.js
@@ -0,0 +1,26 @@
+/* @license Plasttic v2.0 | MIT License | https://plasttic.dev/ */
+
+/**
+ * plasttic.js
+ *
+ * Project/Page scripts
+ * This are scripts that are specific to the project
+ *
+ */
+
+/**
+ * Copy HTML Boilerplate to clipboard
+ * Use: copy code to clipboard on button click
+ * Page: code.html
+ */
+
+export function copyCodeToClipboard(copyCodeBtn) {
+  copyCodeBtn.addEventListener('click', () => {
+    const codeText = document.getElementsByTagName('code')[0].innerText.trim();
+    navigator.clipboard.writeText(codeText);
+    copyCodeBtn.innerText = 'Copied!';
+    setTimeout(() => {
+      copyCodeBtn.innerText = 'Copy';
+    }, 1500);
+  });
+}
