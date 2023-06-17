a6c3b4f583e05c359973d4660c36555144c58f46
Goncalo Tojeiro <dev@tojeiro.me>
2023-06-17 06:24:52 +0100

    v2.1.3: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,14 +10,9 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
     requireConfigFile: false,
-    babelOptions: {
-      presets: ['@babel/preset-react'],
-    },
     ecmaVersion: 2022,
     ecmaFeatures: {
-      jsx: true,
       modules: true,
-      tsx: true,
     },
   },
   env: {
@@ -81,35 +76,6 @@ module.exports = {
     'import/extensions': 0,
     'no-underscore-dangle': 0,
     'consistent-return': 0,
-    'react/display-name': 1,
-    'react/no-array-index-key': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/prefer-stateless-function': 0,
-    'react/forbid-prop-types': 0,
-    'react/no-unescaped-entities': 0,
-    'react/function-component-definition': 0,
-    'jsx-a11y/accessible-emoji': 0,
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        assert: 'either',
-      },
-    ],
-    'react/require-default-props': 0,
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
-      },
-    ],
-    radix: 0,
-    'no-shadow': [
-      2,
-      {
-        hoist: 'all',
-        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
-      },
-    ],
     quotes: [
       2,
       'single',
@@ -129,17 +95,7 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
-    'jsx-a11y/href-no-hash': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'warn',
-      {
-        aspects: ['invalidHref'],
-      },
-    ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/comma-dangle': ['off'],
-    'react/jsx-props-no-spreading': 'off',
   },
-  plugins: ['html', 'prettier', 'react-hooks'],
+  plugins: ['html', 'prettier'],
 };
