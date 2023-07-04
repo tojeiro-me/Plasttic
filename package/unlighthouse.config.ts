e6ad739ba6095353e83865982ef75547b95b0462
Goncalo Tojeiro <dev@tojeiro.me>
2023-07-04 06:18:07 +0100

    v2.3.0: unlighthouse

--- package/unlighthouse.config.ts
+++ package/unlighthouse.config.ts
@@ -1,9 +1,11 @@
-import { loadConfig } from 'unconfig';
-
 export default {
   cache: false,
-  debug: true,
+  debug: false,
   scanner: {
     device: 'mobile',
+    robotsTxt: false,
+    sitemap: true,
+    crawler: true,
   },
+  urls: ['/', '/templates/404.html', '/templates/temporary.html'],
 };
