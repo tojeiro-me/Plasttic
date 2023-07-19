408124a58fd53715767ba1c179c7ba23617d042b
Goncalo Tojeiro <dev@tojeiro.me>
2023-07-19 13:39:50 +0100

    v3.0: css nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,18 +1,12 @@
 module.exports = {
   plugins: [
-    require('postcss-import-ext-glob')({
-      sort: 'asc'
-    }),
+    require('postcss-import-ext-glob')({ sort: 'asc' }),
     require('postcss-import')({ path: 'src/assets/css' }),
-    require('postcss-preset-env')({ 
-      stage: 2,
-      features: {
-        'nesting-rules': true }
-      }),
+    require('postcss-preset-env')({ stage: 2 }),
+    require('postcss-custom-media'),
     require('postcss-media-minmax'),
     require('postcss-mixins'),
     require('postcss-nesting'),
-    // require('postcss-nested'),
     require('postcss-simple-vars'),
     require('autoprefixer'),
     require('postcss-discard-empty'),
